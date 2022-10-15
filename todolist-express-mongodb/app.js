7f24ce9635348c0dec2e38b7f63d754dbff27775
jeremychen <urjjj0909@gmail.com>
2022-10-15 11:45:08 +0800

    modify mongoose config

--- todolist-express-mongodb/app.js
+++ todolist-express-mongodb/app.js
@@ -1,13 +1,13 @@
 // Require express & handlebars module in the project.
 const express = require("express")
-const mongoose = require("mongoose")
 const app = express()
 const port = 5000
 const exphbs = require("express-handlebars")
-const Todo = require("./models/todo") // 載入Todo model
 const bodyParser = require("body-parser") // 引用body-parser
 const methodOverride = require("method-override") // 引用body-parser
 const routes = require("./routes") // 引用路由器
+
+require("./config/mongoose")
 
 // 規定每一筆請求都需要透過body-parser進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -18,17 +18,6 @@ app.use(methodOverride("_method"))
 // Setting template engine.
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }))
 app.set("view engine", "hbs")
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-const db = mongoose.connection
-
-db.on("error", () => {
-    console.log("mongodb error!")
-})
-
-db.once("open", () => {
-    console.log("mongodb connected!")
-})
 
 // 將request導入路由器
 app.use(routes)
--- todolist-express-mongodb/models/seeds/seed.js
+++ todolist-express-mongodb/models/seeds/seed.js
@@ -1,17 +1,9 @@
-const mongoose = require("mongoose")
-const Todo = require("./models/todo") // 載入Todo model
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-const db = mongoose.connection
-
-db.on("error", () => {
-    console.log("mongodb error!")
-})
+const Todo = require("../todo") // 載入Todo model
+const db = require("../../config/mongoose")
 
 db.once("open", () => {
-    console.log("mongodb connected!")
     for (let i=0; i<10; i++) {
         Todo.create({ name: `name-${i}` })
     }
-    console.log('data inserted!')
+    console.log("data inserted!")
 })
