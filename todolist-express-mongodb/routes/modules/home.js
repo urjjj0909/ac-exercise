3ce934df6c81e3da304fbcc33b85d2a852bb4ab3
jeremychen <urjjj0909@gmail.com>
2022-10-15 16:35:27 +0800

    typo

--- todolist-express-mongodb/routes/modules/home.js
+++ todolist-express-mongodb/routes/modules/home.js
@@ -5,10 +5,10 @@ const Todo = require("../../models/todo") // 載入Todo model
 
 // 定義首頁路由
 router.get("/", (req, res) => {
-    Todo.find() // 取出 Todo model 裡的所有資料
-        .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    Todo.find() // 取出Todo model裡的所有資料
+        .lean() // 把Mongoose的Model物件轉換成乾淨的 JavaScript資料陣列
         .sort({ _id: "asc" })
-        .then(todos => res.render("index", { todos: todos })) // 將資料傳給 index 樣板
+        .then(todos => res.render("index", { todos: todos })) // 將資料傳給index樣板
         .catch(error => console.error(error)) // 錯誤處理
 })
 
