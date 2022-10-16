ca4038a432f987d4941408f054deaaedb60f5876
jeremychen <urjjj0909@gmail.com>
2022-10-16 15:17:02 +0800

    add sticky footer and sort function

--- restaurant-express-mongodb/routes/modules/home.js
+++ restaurant-express-mongodb/routes/modules/home.js
@@ -2,25 +2,32 @@
 const express = require("express")
 const router = express.Router()
 const Restaurant = require("../../models/restaurant") // 載入Restaurant model
+const sorting = require("../../utilities/sort")
 
 // 定義首頁路由
 router.get("/", (req, res) => {
+    const sortCondition = { name: "asc" }
+    const sortBy = {
+        isNameAsec: true,
+        isNameDsec: false,
+        isCategory: false,
+        isLocation: false,
+    }
     Restaurant.find() // 取出Restaurant model裡的所有資料
         .lean() // 把Mongoose的Model物件轉換成乾淨的JavaScript資料陣列
-        .sort({ _id: "asc" })
+        .sort(sortCondition)
         // .then(restaurants => console.log(restaurants))
-        .then(restaurants => res.render("index", { restaurants: restaurants })) // 將資料傳給index樣板
+        .then(restaurants => res.render("index", { restaurants: restaurants, keyword: "", sortBy: sortBy })) // 將資料傳給index樣板
         .catch(error => console.error(error)) // 錯誤處理
 })
 
 router.get("/search", (req, res) => {
-    if (!req.query.keyword) {
-        res.redirect("/")
-    }
-    // console.log(req.query.keyword)
     const keywordByUser = req.query.keyword
-    const keyword = req.query.keyword.toLowerCase()
+    const keyword = req.query.keyword.trim().toLowerCase()
+    const sortby = req.query.sortby
+    const [sortCondition, sortBy] = sorting(sortby)
     Restaurant.find()
+        .sort(sortCondition)
         .lean()
         .then(restaurants => {
             const filterRestaurants = restaurants.filter(
@@ -28,7 +35,7 @@ router.get("/search", (req, res) => {
                     return data.name.toLowerCase().includes(keyword) || data.category.includes(keyword)
                 }
             )
-            res.render("index", { restaurants: filterRestaurants, keyword: keywordByUser })
+            res.render("index", { restaurants: filterRestaurants, keyword: keywordByUser, sortBy: sortBy })
         })
         .catch(error => console.log(error))
 })
---
+++ restaurant-express-mongodb/utilities/sort.js
@@ -0,0 +1,34 @@
+function sorting(sortIndex) {
+    const sortOptionsBool = findCategory(sortIndex)
+    switch (sortIndex) {
+        case "1":
+            return [{ name: "asc" }, sortOptionsBool]
+        case '2':
+            return [{ name: "desc" }, sortOptionsBool]
+        case '3':
+            return [{ category: "asc" }, sortOptionsBool]
+        default:
+            return [{ location: "asc" }, sortOptionsBool]
+    }
+}
+
+function findCategory(sortIndex) {
+    const sortCollection = {
+        isNameAsec: false,
+        isNameDsec: false,
+        isCategory: false,
+        isLocation: false,
+    }
+    if (sortIndex === "1") {
+        sortCollection.isNameAsec = true
+    } else if (sortIndex === "2") {
+        sortCollection.isNameDsec = true
+    } else if (sortIndex === "3") {
+        sortCollection.isCategory = true
+    } else if (sortIndex === "4") {
+        sortCollection.isLocation = true
+    }
+    return sortCollection
+}
+
+module.exports = sorting
