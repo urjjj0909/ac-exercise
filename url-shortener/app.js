8b6c522f4ed1561c2836d758791f6d18c4c0e082
jeremychen <urjjj0909@gmail.com>
2022-10-23 20:47:35 +0800

    modify app.js and model function name

--- url-shortener/app.js
+++ url-shortener/app.js
@@ -8,3 +8,23 @@ const methodOverride = require("method-override") // 引用body-parser
 const routes = require("./routes") // 引用路由器
 
 require("./config/mongoose")
+
+// 規定每一筆請求都需要透過body-parser進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// 由於瀏覽器在form中僅看得懂POST方法，規定由form送出的每一筆請求都需要透過method-override將路由導到PUT和DELETE
+app.use(methodOverride("_method"))
+
+// Setting template engine.
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }))
+app.set("view engine", "hbs")
+
+// Setting static files.
+app.use(express.static("public"))
+
+// 將request導入路由器
+app.use(routes)
+
+app.listen(port, () => {
+    console.log(`App is running on http://localhost:${port}`)
+})
--- url-shortener/models/url.js
+++ url-shortener/models/url.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const urlSchema = new Schema({
-    shortURL: { type: String, required: true },
-    originalURL: { type: String, required: true }
+    shortUrl: { type: String, required: true },
+    originalUrl: { type: String, required: true }
 })
 
 module.exports = mongoose.model("Url", urlSchema)
